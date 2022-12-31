defc76cdc9b97cda1adc5a00c9e87330b0eb976d
Ryan Kulp <ryanckulp@gmail.com>
2022-12-31 13:32:48 -0500

    database migrations

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  include Signupable
+  include Onboardable
+  include Billable
+
+  scope :subscribed, -> { where(paying_customer: true) }
+
+  has_many :widgets, dependent: :destroy
+end
---
+++ config/routes.rb
@@ -0,0 +1,35 @@
+Rails.application.routes.draw do
+  resources :widgets
+  root 'pages#home'
+
+  devise_for :users
+  get 'logout', to: 'pages#logout', as: 'logout'
+
+  resources :subscribe, only: [:index]
+  resources :dashboard, only: [:index]
+  resources :account, only: [:index, :update]
+  resources :billing_portal, only: [:create]
+  match '/billing_portal' => 'billing_portal#create', via: [:get]
+  match '/cancel' => 'billing_portal#destroy', via: [:get]
+
+  # static pages
+  pages = %w(
+    privacy terms
+  )
+
+  pages.each do |page|
+    get "/#{page}", to: "pages##{page}", as: "#{page.gsub('-', '_')}"
+  end
+
+  # admin panels
+  authenticated :user, -> user { user.admin? } do
+    namespace :admin do
+      resources :dashboard, only: [:index]
+      resources :impersonations, only: [:new]
+      resources :users, only: [:edit, :update, :destroy]
+    end
+
+    # convenience helper
+    get 'admin', to: 'admin/dashboard#index'
+  end
+end
