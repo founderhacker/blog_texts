6b38f9f260f5e4ae85678d82ff98dc1b8a5e6bf0
Ryan Kulp <ryanckulp@gmail.com>
2023-01-03 15:35:31 -0500

    solution - new user onboarding UX, Messages empty state, and dynamic marketing site widget

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    resource.paying_customer? ? dashboard_path : subscribe_index_path # point these wherever you want
+    resource.widgets.count.zero? ? new_widget_path : dashboard_path
 	end
 
   def maybe_skip_onboarding
-    redirect_to dashboard_path, notice: "You're already subscribed" if current_user.finished_onboarding?
+    redirect_to dashboard_path, notice: "You're already subscribed" if current_user.paying_customer?
   end
 end
